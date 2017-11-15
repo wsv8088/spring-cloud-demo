96cd870b6d21797103da7f5744e2f8d33a866147
shuang_wang <shuang_wang@reachauto.com>
2017-11-15 18:52:46 +0800

    增加monitor和gateway组件

--- cloud-service-user/src/main/java/com/wsun/springcloud/service/user/controller/UserController.java
+++ cloud-service-user/src/main/java/com/wsun/springcloud/service/user/controller/UserController.java
@@ -1,28 +1,23 @@
 package com.wsun.springcloud.service.user.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
-@Slf4j
 public class UserController {
     private static final Logger logger = LogManager.getLogger(UserController.class);
 
-    @Value("${global.db.driver}")
-    private String driver;
-
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String hello() {
-        return "hello" + driver;
+        logger.error("我去:{}", "hello");
+        return "hello";
     }
-
 
     public static void main(String[] args) {
-        logger.error("我去:{}", "这是啥");
+        logger.error("我去:{}", "hello");
     }
+
 }
