973e7766a32ceccc5db77fc73a5948cb08d31d92
shuang_wang <shuang_wang@reachauto.com>
2017-11-24 17:49:03 +0800

    升级SBA版本

---
+++ cloud-service-order/src/main/java/com/wsun/springcloud/service/order/OrderApplication.java
@@ -0,0 +1,17 @@
+package com.wsun.springcloud.service.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
